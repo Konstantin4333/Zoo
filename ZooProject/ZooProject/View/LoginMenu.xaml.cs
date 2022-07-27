fa01f8a4a8218d340cc349672182948c0249e509
Konstantin <kmdimitrov13@gmail.com>
2022-07-27 18:52:14 +0300

    Added Views windows in Views

---
+++ ZooProject/ZooProject/View/LoginMenu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZooProject.View
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class LoginMenu : Window
+    {
+        public LoginMenu()
+        {
+           
+            InitializeComponent();
+             
+        }
+        public void TextBoxPassword()
+        {
+         /*   public char PasswordChar { get; set; }
+        TextBox tbPass = new TextBox();
+        tbPass.PasswordChar = "*";*/
+
+            
+        }
+    }
+}
